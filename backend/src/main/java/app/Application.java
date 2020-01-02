240fc377ce460572eb7850fa2fa466d2612cf049
dvir <dvirdaniel@gmail.com>
2020-01-02 12:07:56 +0200

    Initial Version

---
+++ backend/src/main/java/app/Application.java
@@ -0,0 +1,25 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableCaching
+@EntityScan(basePackages = {"app.persistence.entity"})
+@EnableJpaRepositories(basePackages = {"app.persistence.repository"})
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
